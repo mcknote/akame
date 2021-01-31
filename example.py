08c6aebf775beb8213814a48f81faaae551eb710
Jimmy Lin <im@jmln.io>
2021-01-31 20:30:03 +0800

    minor changes to the example.py

--- example.py
+++ example.py
@@ -10,7 +10,7 @@ pushover_creds = {
 
 if __name__ == "__main__":
     run_task(
-        task_name="Is Time Changing in Taipei?",
+        task_name="Does Time Flow in Taipei?",
         target_url=(r"http://worldtimeapi.org/api/timezone/Asia/Taipei"),
         exset_id="BASIC",
         loop_seconds=30,
